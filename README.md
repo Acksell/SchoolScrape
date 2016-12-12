d91705c162ab140fbeb1879be9d9d0d20c5d38ff
Acksell <axel.engstrom@elev.europaskolan.se>
2016-12-13 00:26:38 +0100

    Fixed encoding issues.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # SchoolScrape
-Logs into the intranet service for schools, Schoolsoft, and scrapes any specific schedule – be it a student's, a class', a teacher's or even a classroom's.
+Logs into an intranet service for schools, Schoolsoft, and scrapes any specific schedule – be it a student's, a class', a teacher's or even a classroom's.
 
-# Requirements
-The third-party module requests
+## Requirements
+The third-party modules [Requests](http://docs.python-requests.org/en/master/) and [Beautiful Soup 4](http://beautiful-soup-4.readthedocs.io/en/latest/).
 
     $ pip install requests
     $ pip install bs4
--- Schoolsoft.py
+++ Schoolsoft.py
@@ -1,4 +1,4 @@
-#~*~ coding: iso-8859-1 ~*~
+#-*- coding: iso-8859-1 -*-
 
 # Schoolsoft API
 # Axel Engström, Europaskolan
@@ -6,6 +6,7 @@
 
 import time
 import requests
+from bs4 import BeautifulSoup
 
 from secrets import USERNAME, PASSWORD
 
@@ -13,20 +14,24 @@ from secrets import USERNAME, PASSWORD
 DEBUG = False
 
 def log(response, *other):
-    '''Logs responses status code, response headers and a slice of the raw html'''
-    loglist = ['%d %s' % (response.status_code,response.reason), response.url, 
+    '''
+    Logs the response's status code, final url destination, history of responses until
+    final destination, received headers and anything else that was given.
+    '''
+    # Opted to not use starred unpacking syntax of python3.5 to make it more backwards 
+    # compatible. The current solution of adding lists is rather ugly though.
+    loglist = ['{} {}'.format(response.status_code, response.reason), response.url, 
                response.history, response.headers] + list(other) + ['-------------------']
     with open('responses.txt','a') as logfile:
         write = lambda *objects: logfile.write(''.join(str(obj)+'\n' for obj in objects))
-        response.encoding = 'ISO-8859-1'
         write(*loglist)
     if DEBUG: 
         for i in loglist[:-1]: print(i)
 
 session = requests.Session()
+
 # Will change when project is finished, right now it resembles a normal browser User-Agent.
 headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'}
-
 ### Authentication
 login_url = 'https://sms3.schoolsoft.se/es/jsp/Login.jsp'
 
@@ -43,6 +48,10 @@ data = {'menu':'schedule'}
 get_schedule = session.get(schedule_url, params=data, headers=headers)
 log(get_schedule)
 
+soup = BeautifulSoup(get_schedule.text, 'html.parser')
+rows = soup.find_all('tr','background schedulerow')
+# encode the result properly and turn into generator
+rows = (row.encode() for row in rows)
 
 # Close connection manually so that the next request
 # doesn't have to wait after Keepalive is dropped. 

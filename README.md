72648ab93a790614329ffbfc1deab525cc6e2aff
Acksell <axel.engstrom@elev.europaskolan.se>
2016-12-12 17:55:13 +0100

    Fixed a typo in the schedule URL which routed to the startpage instead

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SchoolScrape
 Logs into the intranet service for schools, Schoolsoft, and scrapes any specific schedule – be it a student's, a class', a teacher's or even a classroom's.
+
+# Requirements
+The third-party module requests
+
+    $ pip install requests
+    $ pip install bs4
--- Schoolsoft.py
+++ Schoolsoft.py
@@ -4,47 +4,45 @@
 # Axel Engström, Europaskolan
 # 2016-12-06
 
-import requests
-from secrets import USERNAME, PASSWORD
 import time
+import requests
+
+from secrets import USERNAME, PASSWORD
+
 
 DEBUG = False
 
-def log(response, stdout=DEBUG):
-    '''
-    Logs responses status code, response headers and a slice of the raw html
-    '''
+def log(response, *other):
+    '''Logs responses status code, response headers and a slice of the raw html'''
+    loglist = ['%d %s' % (response.status_code,response.reason), response.url, 
+               response.history, response.headers] + list(other) + ['-------------------']
     with open('responses.txt','a') as logfile:
+        write = lambda *objects: logfile.write(''.join(str(obj)+'\n' for obj in objects))
         response.encoding = 'ISO-8859-1'
-        logfile.write(response.text[-200:])
-        logfile.write('\n')
-        logfile.write(str(response.status_code))
-        logfile.write('\n')
-        logfile.write(str(response.headers))
-        logfile.write('\n------------------------------------------\n')
-    if stdout:
-        print(response.text.encode('ascii','xmlcharrefreplace')[-200:])
-        print(response.status_code)
-        print(response.headers)
-        print
+        write(*loglist)
+    if DEBUG: 
+        for i in loglist[:-1]: print(i)
 
 session = requests.Session()
 # Will change when project is finished, right now it resembles a normal browser User-Agent.
 headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'}
 
+### Authentication
 login_url = 'https://sms3.schoolsoft.se/es/jsp/Login.jsp'
 
 data = {'action':'login','usertype':1,'ssusername':USERNAME,'sspassword':PASSWORD,'button':'Logga in'}
 response = session.post(login_url, data=data, headers=headers)
 log(response)
 
-time.sleep(2)
+# Wait until next request
+time.sleep(1)
 
-
-startmenu_url = 'https://sms3.schoolsoft.se/es/jsp/student/right_student_startpage.jsp'
+### Get schedule
+schedule_url = 'https://sms3.schoolsoft.se/es/jsp/student/right_student_schedule.jsp'
 data = {'menu':'schedule'}
-get_schedule = session.get(startmenu_url, params=data, headers=headers)
+get_schedule = session.get(schedule_url, params=data, headers=headers)
 log(get_schedule)
+
 
 # Close connection manually so that the next request
 # doesn't have to wait after Keepalive is dropped. 
